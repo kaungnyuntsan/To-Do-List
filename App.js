3765e1bf35699408a76fae66675a3aed4807b446
Kaung-Nyunt-San <71218665+Kaung-Nyunt-San@users.noreply.github.com>
2023-11-11 22:41:57 +0630

    todo task & delete draft

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+} from "react-native";
 
-export default function App() {
+let id = 0;
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () =>
+    setTasks([...tasks, { id: id++, description: `task ${id}` }]);
+
+  deleteTask = (id) => setTasks(tasks.filter((task) => task.id !== id));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}> To-do lists </Text>
+      <Button title="Add Task" onPress={addTask} />
+      <Button title="console tasks" onPress={() => console.log(tasks)} />
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => (
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.text}> {item.description} </Text>
+            <Button title="delete" onPress={() => deleteTask(item.id)} />
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 30,
+    // borderWidth: 1,
+    padding: 10,
+    marginLeft: 10,
+    height: 50,
   },
 });
+
+export default App;
