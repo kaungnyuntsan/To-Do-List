0e66e6d92ecbcf44e6ab7323f6c17a246ca84627
Kaung-Nyunt-San <71218665+Kaung-Nyunt-San@users.noreply.github.com>
2023-11-22 21:14:43 +0630

    draft sqlite CRUD testing

--- App.js
+++ App.js
@@ -1,13 +1,97 @@
-import React, { useState } from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Button,
+  ScrollView,
+} from "react-native";
 import { Todolists } from "./features/Todolists";
 import { Inputbox } from "./features/Inputbox";
 import { Title } from "./features/Title";
+import * as SQLite from "expo-sqlite";
 
 let id = 0;
 
 const App = () => {
+  const db = SQLite.openDatabase("todolist.db");
   const [tasks, setTasks] = useState([]);
+  const [dbtasks, setDbtasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS todolist (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, isDone INTEGER)"
+      );
+    });
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM todolist",
+        null,
+        (txObj, resultSet) => setDbtasks(resultSet.rows._array),
+        (txObj, error) => console.log(error)
+      );
+    });
+
+    setIsLoading(false);
+  }, []);
+
+  const selectdb = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM todolist",
+        null,
+        (txObj, resultSet) => console.log(resultSet.rows._array),
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const insertdb = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "INSERT INTO todolist (description, isDone) VALUES (?,?) ",
+        ["test1", false],
+        (txObj, resultSet) => console.log(resultSet),
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const updatedb = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "UPDATE todolist SET isDone = ? WHERE id = ?",
+        [1, 2],
+        (txObj, resultSet) => console.log(resultSet),
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const deletedb = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "DELETE FROM todolist WHERE id = ?",
+        [1],
+        (txObj, resultSet) => console.log(resultSet),
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const droptb = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "DROP TABLE todolist",
+        null,
+        (txObj, resultSet) => console.log(resultSet.rows._array),
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
 
   const addTask = (inputData) => {
     setTasks([...tasks, { id: id++, description: inputData, isDone: false }]);
@@ -30,10 +114,44 @@ const App = () => {
     );
   };
 
+  if (isLoading) {
+    return (
+      <SafeAreaView
+        style={[
+          styles.container,
+          { alignItems: "center", justifyContent: "center" },
+        ]}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+          }}
+        >
+          {" "}
+          Loading tasks...{" "}
+        </Text>
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Title tasks={tasks} />
       <Inputbox addTask={addTask} />
+      <Button title="selectdb" onPress={selectdb} />
+      <Button title="insertdb" onPress={insertdb} />
+      <Button title="updatedb" onPress={updatedb} />
+      <Button title="deletedb" onPress={deletedb} />
+      {/* <Button title="droptb" onPress={droptb} /> */}
+      <Button title="console dbtask" onPress={() => console.log(dbtasks)} />
+      <ScrollView>
+        {dbtasks.map((task) => (
+          <Text key={task.id}>
+            {" "}
+            {task.description} {task.isDone}
+          </Text>
+        ))}
+      </ScrollView>
       <Todolists
         tasks={tasks}
         toggleSwitch={toggleSwitch}
