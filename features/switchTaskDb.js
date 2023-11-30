8fc4557d1a62332cbb4467d08f14bd62d76ab219
Kaung-Nyunt-San <71218665+Kaung-Nyunt-San@users.noreply.github.com>
2023-11-30 22:37:44 +0630

    clean up comment

--- features/switchTaskDb.js
+++ features/switchTaskDb.js
@@ -1,28 +1,3 @@
-//  // update database & UI state
-//  const toggleSwitch = (id, isDone) => {
-//     db.transaction((tx) => {
-//       tx.executeSql(
-//         "UPDATE todolist SET isDone = ? WHERE id = ?",
-//         [!isDone, id],
-//         (txObj, resultSet) => {
-//           setTasks(
-//             tasks.map((task) => {
-//               if (task.id === id) {
-//                 return {
-//                   ...task,
-//                   isDone: !task.isDone,
-//                 };
-//               } else {
-//                 return task;
-//               }
-//             })
-//           );
-//         },
-//         (txObj, error) => console.log(error)
-//       );
-//     });
-//   };
-
 const switchTaskDb = (db, id, isDone, setTasks, tasks) => {
   db.transaction((tx) => {
     tx.executeSql(
